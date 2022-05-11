5bd6c75b498b56e5455422a09cc7b331d8a78baf
cat-friend <denisepli@gmail.com>
2022-05-11 13:31:15 -0700

    completed select-from-nobel

--- 03-select-from-nobel.sql
+++ 03-select-from-nobel.sql
@@ -5,19 +5,27 @@ nobel table structure:
 
 -- 1.
 -- Change the query shown so that it displays Nobel prizes for 1950.
-
+SELECT * FROM nobel WHERE yr = 1950
 
 -- 2.
 -- Show who won the 1962 prize for literature.
 
+SELECT winner FROM nobel WHERE yr = 1962 AND subject = 'Literature'
+
 -- 3.
 -- Show the year and subject that won 'Albert Einstein' his prize.
+
+SELECT yr, subject FROM nobel WHERE winner = 'Albert Einstein'
 
 -- 4.
 -- Give the name of the 'peace' winners since the year 2000, including 2000.
 
+SELECT winner FROM nobel WHERE yr >= 2000 AND subject = 'Peace'
+
 -- 5.
 -- Show all details (yr, subject, winner) of the literature prize winners for 1980 to 1989 inclusive.
+
+SELECT * FROM nobel WHERE subject = 'Literature' AND yr BETWEEN 1980 AND 1989
 
 -- 6.
 -- Show all details of the presidential winners:
@@ -27,25 +35,44 @@ nobel table structure:
 -- Jimmy Carter
 -- Barack Obama
 
+SELECT * FROM nobel WHERE winner IN(
+'Theodore Roosevelt',
+'Woodrow Wilson',
+'Jimmy Carter',
+'Barack Obama')
+
 -- 7.
 -- Show the winners with first name John
+SELECT winner FROM nobel WHERE winner LIKE 'John%'
 
 -- 8.
 -- Show the year, subject, and name of physics winners for 1980 together with the chemistry winners for 1984.
 
+SELECT * FROM nobel WHERE subject = 'physics' AND yr = 1980 OR subject = 'chemistry' AND yr = 1984
+
 -- 9.
 -- Show the year, subject, and name of winners for 1980 excluding chemistry and medicine
+SELECT * FROM nobel WHERE yr = 1980 AND subject NOT IN ('chemistry', 'medicine')
+
+-- the above is correct but is shown as incorrect. the prompt and solution do not match. the query below will match what the solution is.
+-- SELECT * FROM nobel WHERE yr = 1980 AND subject NOT IN ('chemistry')
 
 -- 10.
 -- Show year, subject, and name of people who won a 'Medicine' prize in an early year (before 1910, not including 1910) together with winners of a 'Literature' prize in a later year (after 2004, including 2004)
+
+SELECT * FROM nobel WHERE subject = 'Medicine' AND yr < 1910 OR subject = 'literature' AND yr >= 2004
 
 -- 11.
 -- Find all details of the prize won by PETER GRÜNBERG
 -- Non-ASCII characters
 -- The u in his name has an umlaut. You may find this link useful https://en.wikipedia.org/wiki/%C3%9C#Keyboarding
 
+SELECT * FROM nobel WHERE winner LIKE 'Peter Gr_nberg'
+
 -- 12.
 -- Find all details of the prize won by EUGENE O'NEILL
+
+SELECT * FROM nobel WHERE winner = 'Eugene O''Neill'
 
 -- Escaping single quotes
 -- You can't put a single quote in a quote string directly. You can use two single quotes within a quoted string.
@@ -55,7 +82,11 @@ nobel table structure:
 
 -- List the winners, year and subject where the winner starts with Sir. Show the the most recent first, then by name order.
 
+SELECT winner, yr, subject FROM nobel WHERE winner LIKE 'Sir%' ORDER BY yr DESC, winner
+
 -- 14.
 -- The expression subject IN ('chemistry','physics') can be used as a value - it will be 0 or 1.
 
 -- Show the 1984 winners and subject ordered by subject and winner name; but list chemistry and physics last.
+SELECT winner, subject FROM nobel WHERE yr = 1984 ORDER BY
+    CASE WHEN subject IN ('chemistry', 'physics') THEN 1 ELSE 0 END, subject, winner
