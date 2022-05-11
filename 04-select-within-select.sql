94e7ee708390813ed9beb6cf7238048e863fb488
cat-friend <denisepli@gmail.com>
2022-05-11 13:59:17 -0700

    copied and pasted prompts into exercise 4

---
+++ 04-select-within-select.sql
@@ -0,0 +1,52 @@
+-- 1.
+-- List each country name where the population is larger than that of 'Russia'.
+
+SELECT name FROM world WHERE population > (SELECT population FROM world WHERE name = 'Russia')
+
+-- 2.
+-- Show the countries in Europe with a per capita GDP greater than 'United Kingdom'.
+
+-- Per Capita GDP
+-- The per capita GDP is the gdp/population
+
+-- 3.
+
+-- List the name and continent of countries in the continents containing either Argentina or Australia. Order by name of the country.
+
+
+-- 4.
+-- Which country has a population that is more than United Kingom but less than Germany? Show the name and the population.
+
+-- 5.
+-- Germany (population 80 million) has the largest population of the countries in Europe. Austria (population 8.5 million) has 11% of the population of Germany.
+
+-- Show the name and the population of each country in Europe. Show the population as a percentage of the population of Germany.
+
+-- The format should be Name, Percentage for example:
+
+-- name	percentage
+-- Albania	3%
+-- Andorra	0%
+-- Austria	11%
+-- ...	...
+-- Decimal places
+-- You can use the function ROUND to remove the decimal places.
+-- Percent symbol %
+-- You can use the function CONCAT to add the percentage symbol.
+
+-- You need the condition population>0 in the sub-query as some countries have null for population.
+
+-- 6.
+-- Which countries have a GDP greater than every country in Europe? [Give the name only.] (Some countries may have NULL gdp values)
+
+-- 7.
+-- Find the largest country (by area) in each continent, show the continent, the name and the area:
+
+-- 8.
+-- List each continent and the name of the country that comes first alphabetically.
+
+-- 9.
+-- Find the continents where all countries have a population <= 25000000. Then find the names of the countries associated with these continents. Show name, continent and population.
+
+-- 10.
+-- Some countries have populations more than three times that of all of their neighbours (in the same continent). Give the countries and continents.
