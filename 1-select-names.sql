56024d20146371381b19d5ab2744aed02ed2d5de
cat-friend <denisepli@gmail.com>
2022-05-10 19:39:39 -0700

    completed 1-14

--- 1-select-names.sql
+++ 1-select-names.sql
@@ -33,16 +33,21 @@ SELECT name FROM world WHERE name LIKE '%land'
 
 -- Find the countries that start with C and end with ia
 
+SELECT name FROM world WHERE name LIKE 'C%ia'
 
 -- 6.
 -- Greece has a double e - who has a double o?
 
 -- Find the country that has oo in the name
 
+SELECT name FROM world WHERE name LIKE '%oo%'
+
 -- 7.
 -- Bahamas has three a - who else?
 
 -- Find the countries that have three or more a in the name
+
+SELECT name FROM world WHERE name LIKE '%a%a%a%'
 
 -- 8.
 -- India and Angola have an n as the second character. You can use the underscore as a single character wildcard.
@@ -52,33 +57,49 @@ SELECT name FROM world WHERE name LIKE '%land'
 -- ORDER BY name
 -- Find the countries that have "t" as the second character.
 
+SELECT name FROM world WHERE name LIKE '_t%'
+
 -- 9.
 -- Lesotho and Moldova both have two o characters separated by two other characters.
 
 -- Find the countries that have two "o" characters separated by two others.
+
+SELECT name FROM world WHERE name LIKE '%o__o%'
 
 -- 10.
 -- Cuba and Togo have four characters names.
 
 -- Find the countries that have exactly four characters.
 
+SELECT name FROM world WHERE len(name) = 4
+
+--LENGTH() is not a built-in function for SQL
+
 -- 11.
 -- The capital of Luxembourg is Luxembourg. Show all the countries where the capital is the same as the name of the country
 
 -- Find the country where the name is the capital city.
+
+SELECT name FROM world WHERE name = capital
 
 -- 12.
 -- The capital of Mexico is Mexico City. Show all the countries where the capital has the country together with the word "City".
 
 -- Find the country where the capital is the country plus "City".
 
+SELECT name FROM world WHERE capital = CONCAT(name, ' City')
+
 -- 13.
 -- Find the capital and the name where the capital includes the name of the country.
 
+SELECT capital, name FROM world WHERE capital LIKE CONCAT(name, '%')
 -- 14.
 -- Find the capital and the name where the capital is an extension of name of the country.
 
 -- You should include Mexico City as it is longer than Mexico. You should not include Luxembourg as the capital is the same as the country.
+
+SELECT capital, name FROM world WHERE capital LIKE CONCAT(name, '_%')
+
 
 -- 15.
 -- For Monaco-Ville the name is Monaco and the extension is -Ville.
